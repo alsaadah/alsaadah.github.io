c7fa27042aa4d5470aad08f0a89c4227ccfb2d3e
alsaadah <77517374+alsaadah@users.noreply.github.com>
2021-01-16 01:17:44 +0000

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -10,12 +10,41 @@ var app = new Framework7({
     swipe: true,
   },
   // Add default routes
-  routes: [
-    {
-      path: '/pages/about/',
-      url: 'about.html',
-    },
-  ],
+routes = [
+  {
+    path: '/about/',
+    url: './pages/about/about.html',
+    // Pass "tabs" property to route
+    tabs: [
+      // First (default) tab has the same url as the page itself
+      {
+        path: '/',
+        id: 'about',
+        // Fill this tab content from content string
+        content: `
+          <div class="block">
+            <h3>About Me</h3>
+            <p>...</p>
+          </div>
+        `
+      },
+      // Second tab
+      {
+        path: '/contacts/',
+        id: 'contacts',
+        // Fill this tab content via Ajax request
+        url: './pages/about-me/contacts.html',
+      },
+      // Third tab
+      {
+        path: '/cv/',
+        id: 'cv',
+        // Load this tab content as a component via Ajax request
+        componentUrl: './pages/about-me/cv.html',
+      },
+    ],
+  }
+],
   // ... other parameters
 });
 
