b811252da283ab8769e7fa05aa2e489ef5a156c4
alsaadah <77517374+alsaadah@users.noreply.github.com>
2021-01-29 09:00:02 +0000

    Update routes.js

--- docs/js/routes.js
+++ docs/js/routes.js
@@ -1,5 +1,21 @@
 var routes = [
   {
+    path: '/about',
+    url: 'about.html',
+  },
+  {
+    path: '/jasa',
+    url: 'jasa.html',
+  },
+  {
+    path: '/fashion',
+    url: 'fashion.html',
+  },
+  {
+    path: '/preloved',
+    url: 'preloved.html',
+  },
+  {
     path: '/food',
     url: 'food.html',
   },
